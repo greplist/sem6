c8fe6ce6acbde19130496c1f05305d5e43d02ffe
Anton Grabko <softforlutshix@gmail.com>
2017-04-27 00:20:42 +0300

    mtrun: 1: done

--- mtrun/1/1.sh
+++ mtrun/1/1.sh
@@ -5,7 +5,7 @@ set -e
 CC=g++
 CFLAGS="-c -O2 -std=c++14 -g" #-Wall -Wextra
 
-lex 1.lex
+flex 1.lex
 ${CC} ${CFLAGS} lex.yy.c -o lex.yy.c.o
-${CC} lex.yy.c.o -o 1.bin
-./1.bin test.py
+${CC} lex.yy.c.o -o 2.bin
+./2.bin test.py
